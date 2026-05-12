fd15ff33c5c50eab43ecb4eb656ae996e4f74890
FirePixel8422 <123118726+FirePixel8422@users.noreply.github.com>
2026-05-12 15:04:26 +0200

    Added offset position to FastBoxCollider

--- Assets/C# Scripts/Player/FastBoxCollider.cs
+++ Assets/C# Scripts/Player/FastBoxCollider.cs
@@ -7,30 +7,27 @@ public class FastBoxCollider : MonoBehaviour
     [SerializeField] private int id;
 
     [SerializeField] private ColliderType colliderType;
-    [SerializeField] private float3 size = new float3(1);
+    [SerializeField] private float3 halfSize = new float3(1);
+    [SerializeField] private float3 offset = new float3(1);
     
-
     public int Id => id;
     public ColliderType Type => colliderType;
-    public AABB GetAABB()
-    {
-        float3 half = size * 0.5f;
-        float3 center = transform.position;
 
-        return new AABB
-        {
-            Min = center - half,
-            Max = center + half
-        };
+    /// <returns>Lightweight converted collider as AABB with IsActive state</returns>
+    public HitBoxAABB GetAABB()
+    {
+        float3 center = (float3)transform.position + offset;
+
+        return new HitBoxAABB(isActiveAndEnabled, center - halfSize, center + halfSize);
     }
 
 
 #if UNITY_EDITOR
-    public float3 Size => size;
+    public float3 Size => halfSize * 2;
 
-    static readonly Color INVALID_COLOR = new Color(0.6f, 0.2f, 1);
-    static readonly Color HITBOX_COLOR = new Color(0.1f, 0.1f, 1);
-    static readonly Color HURTBOX_COLOR = new Color(1, 0.05f, 0.05f);
+    private static readonly Color INVALID_COLOR = new Color(0.6f, 0.2f, 1);
+    private static readonly Color HITBOX_COLOR = new Color(0.1f, 0.1f, 1);
+    private static readonly Color HURTBOX_COLOR = new Color(1, 0.05f, 0.05f);
 
     private void OnDrawGizmos()
     {
@@ -40,14 +37,24 @@ public class FastBoxCollider : MonoBehaviour
             ColliderType.Hurtbox => HURTBOX_COLOR,
             ColliderType.None or _ => INVALID_COLOR,
         };
-        Gizmos.DrawWireMesh(GlobalMeshes.Cube, transform.position, Quaternion.identity, size);
+        Gizmos.DrawWireMesh(GlobalMeshes.Cube, transform.position, Quaternion.identity, Size);
     }
 #endif
 }
 
 [System.Serializable]
-public struct AABB
+public struct HitBoxAABB
 {
+    private readonly byte activeFlag;
+    public readonly bool IsActive => activeFlag == 1;
+
     public float3 Min;
     public float3 Max;
+
+    public HitBoxAABB(bool active, float3 min, float3 max)
+    {
+        activeFlag = (byte)(active ? 1 : 0);
+        Min = min;
+        Max = max;
+    }
 }
